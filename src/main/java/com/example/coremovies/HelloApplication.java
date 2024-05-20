7685dc82f76010fc2bdfaa7d9a729c1e70efda8f
Jhon Alex <fern@gmail.com>
2024-05-19 20:12:42 -0500

    Añadiendo los nuevos archivos

---
+++ src/main/java/com/example/coremovies/HelloApplication.java
@@ -0,0 +1,29 @@
+package com.example.coremovies;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        /* El programa completo */
+
+        int ancho = 950, largo = 650;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), ancho, largo);
+        stage.getIcons().addAll(new Image("C:\\Users\\johna\\Documents\\Programación\\Java\\core-movies\\src\\main\\resources\\assets\\video-player.png"));
+        stage.setTitle("  John's Movies");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
